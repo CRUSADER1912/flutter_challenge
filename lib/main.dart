e0a5685ffdaa6d932f1e8076330a1b95af023d93
Linus Saldanha <linus.s@peopleinteractive.in>
2018-06-04 11:42:27 +0530

    Updated splash for Android

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: new LandingPage(),
     );
